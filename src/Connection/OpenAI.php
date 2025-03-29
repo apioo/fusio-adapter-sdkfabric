7e8ca1ca06af0c2748aff825f55c8bbb1e7623cd
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-03-29 08:19:38 +0100

    add openai

---
+++ src/Connection/OpenAI.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * Fusio is an open source API management platform which helps to create innovative API solutions.
+ * For the current version and information visit <https://www.fusio-project.org/>
+ *
+ * Copyright (c) Christoph Kappestein <christoph.kappestein@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Fusio\Adapter\SdkFabric\Connection;
+
+use Fusio\Engine\Connection\OAuth2Interface;
+use Fusio\Engine\ConnectionAbstract;
+use Fusio\Engine\Form\BuilderInterface;
+use Fusio\Engine\Form\ElementFactoryInterface;
+use Fusio\Engine\ParametersInterface;
+use SdkFabric\Openai\Client;
+
+/**
+ * OpenAI
+ *
+ * @author  Christoph Kappestein <christoph.kappestein@gmail.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0
+ * @link    https://www.fusio-project.org/
+ */
+class OpenAI extends ConnectionAbstract
+{
+    public function getName(): string
+    {
+        return 'SDK-OpenAI';
+    }
+
+    public function getConnection(ParametersInterface $config): Client
+    {
+        return Client::build($this->getAccessToken($config));
+    }
+
+    public function configure(BuilderInterface $builder, ElementFactoryInterface $elementFactory): void
+    {
+        $builder->add($elementFactory->newInput(OAuth2Interface::CONFIG_ACCESS_TOKEN, 'Access-Token', 'text', 'The Access-Token obtained at the OpenAI admin panel'));
+    }
+
+    protected function getAccessToken(ParametersInterface $config): string
+    {
+        return $config->get(OAuth2Interface::CONFIG_ACCESS_TOKEN) ?? '';
+    }
+}
